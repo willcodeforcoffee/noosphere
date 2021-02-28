aff2b91d59cbcd4e17d00d1b7f0c617320761526
Eric Hoff <eric@hoffsoftcorp.com>
2021-02-28 13:55:07 -0700

    Added a Home and Test controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  get 'home/index'
+  root to: 'home#index'
+
+  get 'test/index' unless Rails.env.production?
 end
