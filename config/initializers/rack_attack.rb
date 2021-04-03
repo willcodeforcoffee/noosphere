c15c7a14fadd9bee8620ac49485c195a7d9cb968
Eric Hoff <eric@hoffsoftcorp.com>
2021-04-03 16:22:38 -0600

    Block login scrapers too

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -22,14 +22,19 @@ module Rack
 
     # Lockout IP addresses that are hammering your login page.
     # After 20 requests in 1 minute, block all requests from that IP for 1 hour.
-    # Rack::Attack.blocklist('allow2ban login scrapers') do |req|
-    #   # `filter` returns false value if request is to your login page (but still
-    #   # increments the count) so request below the limit are not blocked until
-    #   # they hit the limit.  At that point, filter will return true and block.
-    #   Rack::Attack::Allow2Ban.filter(req.ip, maxretry: 20, findtime: 1.minute, bantime: 1.hour) do
-    #     # The count for the IP is incremented if the return value is truthy.
-    #     req.path == '/login' and req.post?
-    #   end
-    # end
+    Rack::Attack.blocklist("allow2ban login scrapers") do |req|
+      # `filter` returns false value if request is to your login page (but still
+      # increments the count) so request below the limit are not blocked until
+      # they hit the limit.  At that point, filter will return true and block.
+      Rack::Attack::Allow2Ban.filter(
+        req.ip,
+        maxretry: 20,
+        findtime: 1.minute,
+        bantime: 1.hour,
+      ) do
+        # The count for the IP is incremented if the return value is truthy.
+        req.path == "/user_credentials/sign_in" and req.post?
+      end
+    end
   end
 end
