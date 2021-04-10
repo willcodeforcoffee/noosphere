4e6777545826e6c1e888ef40e173ba50e731a77b
Eric Hoff <eric@hoffsoftcorp.com>
2021-04-10 08:48:02 -0600

    Updated component to handle strings as well as symbols

--- app/components/alert_component.rb
+++ app/components/alert_component.rb
@@ -6,24 +6,20 @@ class AlertComponent < ViewComponent::Base
     end
   end
 
-  LEVEL = [
-    :error,
-    :warning,
-    :info,
-  ].freeze
+  LEVEL = %i[error warning info].freeze
 
   LEVEL_CSS_CLASSES = {
-    error: 'border-red-300 bg-red-200 text-red-900',
-    warning: 'border-yellow-300 bg-yellow-200 text-yellow-900',
-    info: 'border-green-300 bg-green-200 text-green-900',
+    error: "border-red-300 bg-red-200 text-red-900",
+    warning: "border-yellow-300 bg-yellow-200 text-yellow-900",
+    info: "border-green-300 bg-green-200 text-green-900",
   }.freeze
 
   def initialize(level:, message:)
-    raise InvalidLevelError, level unless LEVEL.include?(level)
+    raise InvalidLevelError, level unless LEVEL.include?(level.to_sym)
     super
 
     @level = level
     @message = message
-    @level_css_class = LEVEL_CSS_CLASSES[level]
+    @level_css_class = LEVEL_CSS_CLASSES[level.to_sym]
   end
 end
