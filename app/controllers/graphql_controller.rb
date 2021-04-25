9592be7664e2406821361183488dc699b35b8bd1
Eric Hoff <eric@hoffsoftcorp.com>
2021-04-25 07:03:56 -0600

    Prettier cleanup

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -10,9 +10,15 @@ class GraphqlController < ApplicationController
     operation_name = params[:operationName]
     context = {
       # Query context goes here, for example:
-      # current_user: current_user,
+      current_user: current_user,
     }
-    result = NoosphereSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result =
+      NoosphereSchema.execute(
+        query,
+        variables: variables,
+        context: context,
+        operation_name: operation_name,
+      )
     render json: result
   rescue StandardError => e
     raise e unless Rails.env.development?
@@ -22,14 +28,11 @@ class GraphqlController < ApplicationController
   private
 
   # Handle variables in form data, JSON body, or a blank value
+  # rubocop:disable Metrics/MethodLength temporarily until this can get refactored
   def prepare_variables(variables_param)
     case variables_param
     when String
-      if variables_param.present?
-        JSON.parse(variables_param) || {}
-      else
-        {}
-      end
+      variables_param.present? ? JSON.parse(variables_param) || {} : {}
     when Hash
       variables_param
     when ActionController::Parameters
@@ -41,10 +44,16 @@ class GraphqlController < ApplicationController
     end
   end
 
+  # rubocop:enable Metrics/MethodLength
+
   def handle_error_in_development(e)
     logger.error e.message
     logger.error e.backtrace.join("\n")
 
-    render json: { errors: [{ message: e.message, backtrace: e.backtrace }], data: {} }, status: :internal_server_error
+    render json: {
+             errors: [{ message: e.message, backtrace: e.backtrace }],
+             data: {},
+           },
+           status: :internal_server_error
   end
 end
